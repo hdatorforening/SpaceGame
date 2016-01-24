580e87298a9d323c588ebb8a2fe9c5ea8c08e9bd
hdatorforening <datorforeningenh@gmail.com>
2016-01-24 16:53:27 +0100

    Flyttade genererings kod från main till generation och NumberFunctions.

--- SpaceGame/SpaceGame/SpaceGame.cpp
+++ SpaceGame/SpaceGame/SpaceGame.cpp
@@ -1,163 +1,20 @@
 // SpaceGame.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
+#include "stdafx.h" 
 #include <iostream>
 #include <cstdlib>
 #include <time.h>
 #include <random>
-#include <cmath>
 #include <map>
 #include <iomanip>
 #include <string>
-#include <math.h> 
+
+
+#include "generation.h"
+
 using namespace std;
 
-
-float normaldist(int type)
-{
-	float nordis = 0, u = 0;
-	for (int i = 0; i < 10; i++) {
-		u = ((rand() % 101) / 500.0);
-		nordis = nordis + u;
-	}
-
-	return nordis;
-}
-
-float planetrot()
-{
-	float planetRotation, f;
-	planetRotation = normaldist(1);
-	f = rand() % 4 + 4;
-
-	planetRotation = planetRotation*1.1;
-
-	cout << planetRotation << "\n";
-	cout << f << "\n";
-
-	planetRotation = pow(planetRotation, f);
-	cout << planetRotation << "\n";
-	return planetRotation;
-}
-
-int* generateSystem()
-{
-	int u = 0, i = 0, objectsInSpace = 0, a[30] = {};
-
-	//Genererar föremål
-	objectsInSpace = rand() % 15 + 1;
-	cout << "Objects: " << objectsInSpace << "\n";
-	while (objectsInSpace > 0) {
-
-
-		//Rocky Planets
-		while ((objectsInSpace > 0) && (i < 30)) {
-			u = rand() % 100 + 1;
-			if (a[i] != 0) {
-
-			}
-			else if (i <= 10) {
-				if (u <= 10) {
-					a[i] = 1;
-					objectsInSpace--;
-				}
-			}
-			else {
-				if (u <= 2) {
-					a[i] = 1;
-					objectsInSpace--;
-				}
-			}
-			i++;
-		}
-
-		i = 0;
-
-		//Gas Planets
-		while ((objectsInSpace > 0) && (i < 30)) {
-			u = rand() % 100 + 1;
-
-			if (a[i] != 0) {
-
-			}
-			else if (i <= 10) {
-				if (u <= 1) {
-					a[i] = 2;
-					objectsInSpace--;
-				}
-			}
-			else {
-				if (u <= 5) {
-					a[i] = 2;
-					objectsInSpace--;
-				}
-			}
-			i++;
-		}
-
-		i = 0;
-
-		//Asteroids
-		while ((objectsInSpace > 0) && (i < 30)) {
-			u = rand() % 100 + 1;
-
-			if (a[i] != 0) {
-
-			}
-			else if (i <= 7) {
-				if (u <= 2) {
-					a[i] = 3;
-					objectsInSpace--;
-				}
-			}
-			else if (i <= 11) {
-				if (u <= 10) {
-					a[i] = 3;
-					objectsInSpace--;
-				}
-			}
-			else if (i <= 23) {
-				if (u <= 5) {
-					a[i] = 3;
-					objectsInSpace--;
-				}
-			}
-			else {
-				if (u <= 15) {
-					a[i] = 3;
-					objectsInSpace--;
-				}
-			}
-			i++;
-		}
-
-		i = 0;
-	}
-	cout << "\n";
-	i = 0;
-	while (i < 30) {
-		cout << a[i] << " ";
-		i++;
-	}
-
-	i = 0;
-	return a;
-}
-
-float generateSystemObject(int obj)
-{
-	int u = 0;
-	if (obj == 1)
-	{
-		float sta[12];
-		sta[0] =
-			sta[0] = (rand() % 1000 + 1) / 100; //ger massa
-												//atmosphere
-
-	}
-	return 0;
-}
 
 void main()
 {
@@ -167,8 +24,15 @@ void main()
 
 	int *arr = generateSystem();
 
-
-	cout << "\n" << arr[2] << "\n";
+	/*int i = 0;
+	while (i < 1) {
+		cout << arr[1];
+		i++;
+	}*/
+	cout << "\n" << arr[2];
+	cout << "\n" << arr[3];
+	cout << "\n" << arr[2];
+	cout << "\n" << arr[3] << "\n";
 
 	system("pause");
 }
---
+++ SpaceGame/SpaceGame/generation.h
@@ -0,0 +1,191 @@
+
+#include "stdafx.h"
+#include <math.h> 
+#include <cmath>
+#include <iostream>
+#include <random>
+#include <time.h>
+
+#include "NumberFunctions.h"
+
+
+using namespace std;
+
+
+int* generateSystem()
+{
+	int u = 0, i = 0, objectsInSpace = 0, a[30] = {};
+
+	//Genererar föremål
+	objectsInSpace = rand() % 15 + 1;
+	cout << "Objects: " << objectsInSpace << "\n";
+	while (objectsInSpace > 0) {
+
+
+		//Rocky Planets
+		while ((objectsInSpace > 0) && (i < 30)) {
+			u = rand() % 100 + 1;
+			if (a[i] != 0) {
+
+			}
+			else if (i <= 10) {
+				if (u <= 10) {
+					a[i] = 1;
+					objectsInSpace--;
+				}
+			}
+			else {
+				if (u <= 2) {
+					a[i] = 1;
+					objectsInSpace--;
+				}
+			}
+			i++;
+		}
+
+		i = 0;
+
+		//Gas Planets
+		while ((objectsInSpace > 0) && (i < 30)) {
+			u = rand() % 100 + 1;
+
+			if (a[i] != 0) {
+
+			}
+			else if (i <= 10) {
+				if (u <= 1) {
+					a[i] = 2;
+					objectsInSpace--;
+				}
+			}
+			else {
+				if (u <= 5) {
+					a[i] = 2;
+					objectsInSpace--;
+				}
+			}
+			i++;
+		}
+
+		i = 0;
+
+		//Asteroids
+		while ((objectsInSpace > 0) && (i < 30)) {
+			u = rand() % 100 + 1;
+
+			if (a[i] != 0) {
+
+			}
+			else if (i <= 7) {
+				if (u <= 2) {
+					a[i] = 3;
+					objectsInSpace--;
+				}
+			}
+			else if (i <= 11) {
+				if (u <= 10) {
+					a[i] = 3;
+					objectsInSpace--;
+				}
+			}
+			else if (i <= 23) {
+				if (u <= 5) {
+					a[i] = 3;
+					objectsInSpace--;
+				}
+			}
+			else {
+				if (u <= 15) {
+					a[i] = 3;
+					objectsInSpace--;
+				}
+			}
+			i++;
+		}
+
+		i = 0;
+	}
+	cout << "\n";
+	i = 0;
+
+	return a;
+}
+
+float generateSystemObject(int obj)
+{
+	int u = 0;
+	if (obj == 1)
+	{
+		float sta[12];
+		sta[0] =
+			sta[0] = (rand() % 1000 + 1) / 100; //ger massa
+												//atmosphere
+
+	}
+	return 0;
+}
+
+//Generate resourses, 1 = Stone Planet, 2 = Gas Planet, 3 = Asteroid, 4 = Moon
+int generateResourses(int type, int pos, float mass)
+{
+
+	/*
+	0 - Metal
+	1 - Rare Earth
+	2 - Water
+	*/
+	float res[3];
+	int total = 0;
+
+
+	//Stone PLanets
+	if (type == 1) {
+		res[0] = 900000000 * (rand() % 1 + 0.5);
+		res[1] = 100000000 * (rand() % 1 + 0.5);
+		res[2] = 50000 * (rand() % 20 - 10);
+		if (res[2] < 0)
+			res[2] = 0;
+	}
+
+	//Gas Planets
+	else if (type == 2) {
+
+	}
+
+	//Asteroids
+	else if (type == 3) {
+
+	}
+
+	//Moons
+	else if (type == 4) {
+
+	}
+
+	for (int i = 0; i < 3; i++) {
+		total += res[i];
+
+		cout << total << ", ";
+	}
+
+	
+
+
+	return -1;
+}
+
+float planetrot()
+{
+	float planrot, f;
+	planrot = normaldist(1);
+	f = rand() % 4 + 4;
+
+	planrot = planrot*1.1;
+
+	cout << planrot << "\n";
+	cout << f << "\n";
+
+	planrot = pow(planrot, f);
+	cout << planrot << "\n";
+	return planrot;
+}
